a3e3f6000173ae9ef0b230524b0cde9518859632
Manash Kumar Mandal <manash@protonmail.com>
2017-03-19 18:07:22 +0600

    fix indentation

--- tsne_plot.py
+++ tsne_plot.py
@@ -33,19 +33,19 @@ embedding = embedding.reshape(limit, vector_dim)
 
 
 def plot_with_labels(low_dim_embs, labels, filename='tsne.png'):
-  assert low_dim_embs.shape[0] >= len(labels), "More labels than embeddings"
-  plt.figure(figsize=(18, 18))  # in inches
-  for i, label in enumerate(labels):
-    x, y = low_dim_embs[i, :]
-    plt.scatter(x, y)
-    plt.annotate(label,
+    assert low_dim_embs.shape[0] >= len(labels), "More labels than embeddings"
+    plt.figure(figsize=(18, 18))  # in inches
+    for i, label in enumerate(labels):
+        x, y = low_dim_embs[i, :]
+        plt.scatter(x, y)
+        plt.annotate(label,
                  xy=(x, y),
                  xytext=(5, 2),
                  textcoords='offset points',
                  ha='right',
                  va='bottom')
 
-  plt.savefig(filename)
+    plt.savefig(filename)
 
 
 # Creating the tsne plot [Warning: will take time]
